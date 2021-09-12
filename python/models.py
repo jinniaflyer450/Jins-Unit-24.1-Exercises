e2c82b2d2c92ab8b86c63639cda50b241f12b456
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-12 17:03:45 -0500

    filled out Pet model in models.py

--- python/models.py
+++ python/models.py
@@ -7,6 +7,16 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
-    
+
 class Pet(db.Model):
     """A model that represents a pet animal."""
+    
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(30), nullable=False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    availability = db.Column(db.Boolean, nullable=False, default=True)
