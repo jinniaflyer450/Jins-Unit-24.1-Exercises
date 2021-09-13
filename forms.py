811d63c3af5f51febbf5f38bc887daa9ece59c0d
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-13 18:59:46 -0500

    updated add pet form to include validators

--- forms.py
+++ forms.py
@@ -4,16 +4,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import Pet
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, FloatField
+from wtforms import StringField, BooleanField, FloatField, URLField, SelectField
+from wtforms.validators import InputRequired, Optional, NumberRange, URL, AnyOf
 
 class AddPetForm(FlaskForm):
     """A form for adding a new pet to the adoption website's database."""
-    name = StringField("Name")
-    species = StringField("Species")
+    name = StringField("Name", validators=[InputRequired()])
+    species = SelectField("Species", choices=[("cat", "cat"), ("dog", "dog"), ("porcupine", "porcupine")], 
+    validators=[AnyOf(["dog", "cat", "porcupine"])])
     availability = BooleanField("Available for adoption?")
-    picture = StringField("Photo")
-    age = FloatField("Age")
-    notes = StringField("Notes")
+    picture = URLField("Photo", validators=[Optional(), URL()])
+    age = FloatField("Age (between 0 and 30)", 
+    validators=[Optional(), NumberRange(min=0, max=30, message="Age must be between 0 and 30 years old.")])
+    notes = StringField("Notes", validators=[Optional()])
 
 class EditPetForm(FlaskForm):
     """A form for editing a pet that is already in the adoption website's database."""
